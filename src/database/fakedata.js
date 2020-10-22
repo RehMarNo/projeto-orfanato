e39e19c5b0bc9ab5b8f2d02c67bde7a525ec81f7
Renata Marques <renatamarquesdl@gmail.com>
2020-10-21 21:45:25 -0300

    Back-end

---
+++ src/database/fakedata.js
@@ -0,0 +1,30 @@
+module.exports = [
+    {
+        id: 1, 
+        lat: "-7.107902",
+        lng: "-34.8871213",
+        name: "Casa Pequeno Davi",
+        description: "A Casa Pequeno Davi, organização não governamental sem fins lucrativos que funciona desde 1985 no Baixo Roger, tem como MISSÃO contribuir para efetivação dos direitos humanos, em especial crianças e adolescentes em vulnerabilidade social, com ações de educação integral, articulação comunitária e institucional e intervenção nos espaços de políticas públicas da Paraíba, numa perspectiva de desenvolvimento sustentável.",
+        images: [
+            "https://images.unsplash.com/photo-1600711724564-526eda91ac29?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+            "https://images.unsplash.com/photo-1600712243809-7a3dd4e68fff?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9"
+        ],
+        instructions: "Venha como se sentir a vontade e traga muito amor e paciência para dar.",
+        opening_hours: "Horário de Visitar Das 8h até 18h",
+        open_on_weekends: "1"
+    },
+    {
+        id: 2, 
+        lat: "-7.1185347",
+        lng: "-34.8814879",
+        name: "Lar das meninas",
+        description: "Presta assistência a crianças de 6 a 15 anos",
+        images: [
+            "https://images.unsplash.com/photo-1600711724564-526eda91ac29?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+            "https://images.unsplash.com/photo-1600712243809-7a3dd4e68fff?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9"
+        ],
+        instructions: "Venha como se sentir a vontade e traga muito amor e paciência para dar.",
+        opening_hours: "Horário de Visitar Das 8h até 18h",
+        open_on_weekends: "0"
+    }
+]
